1a3f955f294fe60e03d5f2b2b79a1ffabff3b627
RichardPFernandes <fernandesrichard312@gmail.com>
2024-04-25 20:20:28 -0300

    feat(init): init repository

---
+++ src/components/CardMedicamento.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/CardMedicamento.css";
+export default function CardMedicamento(props) {
+  return (
+    <div className="card_medicamento">
+      <img src={props.imagem} alt={props.nome} />
+      <h2>{props.nome}</h2>
+      <p>{props.dosagem}</p>
+    </div>
+  );
+}
---
+++ src/routes/PaginaInicial.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import "../styles/PaginaInicial.css";
+import CardFarmacia from "../components/CardFarmacia";
+import catarinense from "../assets/catarinense.png";
+import nissei from "../assets/nissei.png";
+import panvel from "../assets/panvel.png";
+import precoPopular from "../assets/precoPopular.png";
+import pagueMenos from "../assets/pagueMenos.png";
+import { Button } from "react-bootstrap";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import GridViewIcon from "@mui/icons-material/GridView";
+import AddIcon from "@mui/icons-material/Add";
+import AutoAwesomeMotionIcon from "@mui/icons-material/AutoAwesomeMotion";
+import paracetamol from "../assets/paracetamol.png";
+import CardMedicamento from "../components/CardMedicamento";
+
+const medicamentos = [
+  {
+    nome: "Paracetamol",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Dipirona",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Ibuprofeno",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Dorflex",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Doril",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Novalgina",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Buscopan",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Omeprazol",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Ranitidina",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Dorflex",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  },
+  {
+    nome: "Doril",
+    imagem: paracetamol,
+    dosagem: "500mg",
+  }
+];
+
+export default function PaginaInicial() {
+  return (
+    <div className="paginaInicial">
+      <div className="buttons conteudo">
+        <Button variant="light" className="button-nav">
+          <div className="icon">
+            <GridViewIcon
+              style={{
+                color: "var(--hover-color)",
+                fontSize: "6rem",
+                width: "2.5rem",
+              }}
+            />
+          </div>
+          <span>TODOS OS DEPARTAMENTOS</span>
+        </Button>
+        <Button variant="light" className="button-nav">
+          <div className="icon">
+            <AddIcon
+              style={{
+                color: "var(--hover-color)",
+                fontSize: "6rem",
+                width: "3rem",
+              }}
+            />
+          </div>
+          <span>MAIS BUSCADO</span>
+        </Button>
+        <Button variant="light" className="button-nav">
+          <div className="icon">
+            <AutoAwesomeMotionIcon
+              style={{
+                color: "var(--hover-color)",
+                fontSize: "2.5rem",
+                width: "3rem",
+              }}
+            />
+          </div>
+          <span>MARCAS SELECIONADAS</span>
+        </Button>
+        <Button variant="light" className="button-nav">
+          <AddIcon
+            style={{
+              color: "var(--primary-color)",
+              fontSize: "6rem",
+              width: "3rem",
+            }}
+          />
+          <span>FARMACIAS PRÓXIMAS</span>
+        </Button>
+      </div>
+      <div className="farmacia conteudo">
+        <h1>Principais Farmácias</h1>
+        <div className="farmacias">
+          <CardFarmacia imagem={catarinense} nome="Catarinense" />
+          <CardFarmacia imagem={nissei} nome="Nissei" />
+          <CardFarmacia imagem={panvel} nome="Panvel" />
+          <CardFarmacia imagem={precoPopular} nome="Preço Popular" />
+          <CardFarmacia imagem={pagueMenos} nome="Pague Menos" />
+        </div>
+      </div>
+      <div className="medicamentos_div conteudo">
+        <h1>Os Mais Buscados</h1>
+        <div className="carrousel">
+          <div className="button-right">
+            <Button
+              variant="light"
+              style={{ background: "var(--secundary-color)" }}
+            >
+              <ChevronLeftIcon style={{ color: "var(--primary-color)" }} />
+            </Button>
+          </div>
+          <div className="medicamentos">
+            {medicamentos.map((medicamento) => (
+              <CardMedicamento
+                imagem={medicamento.imagem}
+                nome={medicamento.nome}
+                dosagem={medicamento.dosagem}
+              />
+            ))}
+          </div>
+          <div className="button-left">
+            <Button
+              variant="light"
+              style={{ background: "var(--secundary-color)" }}
+            >
+              <ChevronRightIcon style={{ color: "var(--primary-color)" }} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
