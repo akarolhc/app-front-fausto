91ae85cf917e80df18587d5e7c0b6ddf9cecd7ff
vinicius.dev <72517355+viniciusdevegili@users.noreply.github.com>
2024-04-30 21:37:43 -0300

    tela finalizada

--- src/routes/BuscaMedicamento.jsx
+++ src/routes/BuscaMedicamento.jsx
@@ -1,5 +1,7 @@
 import { Margin, Padding } from "@mui/icons-material";
 import "../styles/BuscaMedicamento.css";
+import paracetamol from "../assets/paracetamol.png";
+import heart from "../assets/heart.png";
 
 export default function BuscaMedicamento() {
   return (
@@ -79,7 +81,220 @@ export default function BuscaMedicamento() {
         </div>
       </div>
       <div className="right">
-        <div className="medicamentos-right"></div>
+        <div className="medicamentos-right">
+          <div className="medicamentos-zona">
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+                <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+            <div className="medicamentos-card">
+              <div className="medicamento-favorito">
+              <img src={heart} alt="heart"  width={40} height={40}/>
+              </div>
+              <div className="medicamentos-foto">
+                <img src={paracetamol} alt="paracetamol"  width={185}/>
+              </div>
+              <div className="medicamentos-conteudo">
+                <div className="medicamentos-texto">
+                  <a>Paracetamol</a><br/>
+                  <a>750mg</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
